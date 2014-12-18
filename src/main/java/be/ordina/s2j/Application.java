6dd8e8795e5c42c0f1341536c29d4561b7188ae5
Tom Causs <tom.causs@gmail.com>
2014-12-18 15:38:49 +0100

    Finishing touches

--- src/main/java/be/ordina/s2j/Application.java
+++ src/main/java/be/ordina/s2j/Application.java
@@ -2,8 +2,11 @@ package be.ordina.s2j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import be.ordina.s2j.service.BeerService;
 
 @Configuration
 @ComponentScan
@@ -11,6 +14,12 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+    	
+        BeerService beerService = ctx.getBean(BeerService.class);
+        
+        // export db into a JSON file
+        beerService.export();
+        
     }
 }
--- src/main/java/be/ordina/s2j/repository/BeerRepository.java
+++ src/main/java/be/ordina/s2j/repository/BeerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import be.ordina.s2j.model.Beer;
 
+/**
+ * Repository for {@link Beer}
+ */
 public interface BeerRepository extends JpaRepository<Beer, Integer> {
 
 	

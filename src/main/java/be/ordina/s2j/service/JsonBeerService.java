1bfc3c66809602c139c468f7f02f8b2313c7ba75
Tom Causs <tom.causs@gmail.com>
2014-12-30 14:03:24 +0100

    Adjust output format for bulk indexing

--- src/main/java/be/ordina/s2j/service/JsonBeerService.java
+++ src/main/java/be/ordina/s2j/service/JsonBeerService.java
@@ -1,5 +1,6 @@
 package be.ordina.s2j.service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,8 +23,13 @@ import be.ordina.s2j.util.JsonUtil;
 @Slf4j
 public class JsonBeerService implements BeerService {
 
-	private static final String FILE_NAME = "s2j.json.filename";
-	private static final String DEFAULT_FILE_NAME = "output.json";
+	private static final String PATHNAME = "s2j.json.pathname";
+	private static final String DEFAULT_PATHNAME = ".";
+	private static final String FILENAME = "s2j.json.filename";
+	private static final String DEFAULT_FILENAME = "output.json";
+	
+	private static final String INDEX_NAME = "inventory";
+	private static final String DOC_TYPE = "beer";
 
 	@Autowired
 	private Environment env;
@@ -37,16 +43,28 @@ public class JsonBeerService implements BeerService {
 	@Override
 	public void export() {
 		List<Beer> beerList = beerRepository.findAll();
-		int count = beerList.size();
 		
-		String jsonOutput = JsonUtil.toJson(beerList);
-		String fileName = env.getProperty(FILE_NAME, DEFAULT_FILE_NAME);
+		writeToFile(beerList);
+	}
+	
+	private void writeToFile(List<Beer> list) {
+		int count = list.size();
+		
+		String pathname = env.getProperty(PATHNAME, DEFAULT_PATHNAME);
+		String filename = env.getProperty(FILENAME, DEFAULT_FILENAME);
+		String fullPath = pathname + File.separator + filename;
+
+		StringBuilder sb = new StringBuilder();
+		for (Beer beer : list) {
+			sb.append("{ \"index\" : { \"_index\" : \"" + INDEX_NAME + "\", \"_type\" : \"" + DOC_TYPE + "\" } }\n");
+			sb.append(JsonUtil.toJson(beer) + "\n");
+		}
 		
 		try {
-			Files.write(Paths.get("./" + fileName), jsonOutput.getBytes());
-			log.info("Exporting {} entities into JSON file '{}'", count, fileName);
+			Files.write(Paths.get(fullPath), sb.toString().getBytes());
+			log.info("Exporting {} entities into JSON file '{}'", count, fullPath);
 		} catch (IOException e) {
-			log.error("Failed writing to the file");
+			log.error("Failed writing to the file '{}'", fullPath);
 		}
 	}
 
